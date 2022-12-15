8c6a174115151509e0560231caff0dd3d6165cd3
elite_hzy <892621684@qq.com>
2022-12-15 11:50:38 +0800

    跨域

---
+++ EMI-SpringBoot/EMI-SpringBoot-gateway/src/main/java/EMISpringBoot/gateway/config/CorsConfig.java
@@ -0,0 +1,28 @@
+package EMISpringBoot.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource (new PathPatternParser());
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        // 允许所有请求方法
+        corsConfig.addAllowedMethod ("*");
+        // 允许所有域，当请求头
+        corsConfig.addAllowedOrigin ("*");
+        // 允许全部请求头
+        corsConfig.addAllowedHeader ("*");
+        // 允许携带 Cookie 等用户凭证
+        corsConfig.setAllowCredentials (true);
+        // 允许全部请求路径
+        source.registerCorsConfiguration ("/**", corsConfig);
+        return new CorsWebFilter (source);
+    }
+}
